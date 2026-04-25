8ca2fceb85f243986b945c09d7e3fb1049aefa5b
saravanan642 <saravanansenthil605@gmail.com>
2026-04-25 21:25:56 +0530

    update for loop

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.listen(port, () => {
 
 
 
-const a= 20
-const b=30
-const value = a+b 
-console.log(value);
+// const a= 20
+// const b=30
+// const value = a+b 
+// console.log(value);
 
 // app.get("/",(req,res)=>{
 //     res.send("tha values is " + value)
@@ -28,6 +28,43 @@ console.log(value);
 //     res.send(result);
 // });
 
-for(i=1;i<10; i++){
-  console.log(i + " x 4 "+ "= " + (4 * i));
-}
+// for(i=1;i<10; i++){
+//   console.log(i + " x 4 "+ "= " + (4 * i));
+// }
+// let value = 0;
+
+// for (let i = 0; i < 10; i++) {
+//     let store = value + 1;
+//     console.log(store);
+//     value = store;
+// }
+
+//   let start =" "
+//  for (i=0; i<5; i++){
+//    start = start + "*"
+//    start =start
+//    console.log (start);
+// }
+
+// let num = "";
+// for ( i =0 ; i<=5; i++){
+//     start = num + "*"
+//     console.log(start);
+// }
+
+// let a = 2;
+// let b=3;
+// let fact= a+b; 
+
+// for (i=1; i<=10; i++){
+// fact *=i
+// console.log(fact);
+// }
+
+const x =3;
+const y =2;
+var total = x+y
+ for(i=0; i<10; i++){
+var total= add +=i
+  console.log(total)
+ }
